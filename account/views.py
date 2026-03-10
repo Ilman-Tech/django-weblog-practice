2b738267e2633a4794e179cf5f6a9f45652c7afc
Ilman-Tech <ilmanak.source@gmail.com>
2026-03-11 01:26:58 +0330

    add login BK in project

--- account/views.py
+++ account/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-def login(request):
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username.endswith('.com') and '@' in username:
+            user = authenticate(email=username, password=password)
+        else:
+            user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
     return render(
         request,
         'account/login.html',
